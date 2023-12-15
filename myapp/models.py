360849b8c003e5eff776221f2e7955ad5fba97d4
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2023-12-16 00:15:00 +0530

    Update models.py - Added models as per specification

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class sampleModel(models.Model):
+class newsArticles(models.Model):
+    sourceURL = models.CharField(max_length=100)
+    articleTitle = models.CharField(max_length=100)
+    articleContent = models.CharField(max_length=10000)
+    imageURL = models.CharField(max_length=100, null=True)
+    status = models.CharField(max_length=20, default="Unverified")
+
+class socialMediaPosts(models.Model):
+    sourceURL = models.CharField(max_length=100)
+    articleContent = models.CharField(max_length=10000)
+    imageURL = models.CharField(max_length=100, null=True)
+    platformName = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, default="Unverified")
+    
+class userSelected(models.Model):
     selectedContent = models.CharField(max_length=100)
+    confidence = models.IntegerField()
+    imageURL = models.CharField(max_length=100, null=True)
+    sourceURL = models.CharField(max_length=100)
+    proofURL = models.CharField(max_length=100)
     userFeedback = models.CharField(max_length=100)
-    result = models.CharField(max_length=20, default="Unverified")
+    status = models.CharField(max_length=20, default="Unverified")
