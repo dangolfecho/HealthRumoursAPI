1bc580186741d6617b475938a821fa17e1878f99
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2023-12-16 00:29:05 +0530

    Update views.py - Added new routes to insert data for news articles, and social media posts.

--- myapp/views.py
+++ myapp/views.py
@@ -1,34 +1,69 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import sampleModel
+from .models import *
 from django.views.decorators.csrf import csrf_exempt
 import json
 
 # Create your views here.
 @csrf_exempt
-def insert_data(request):
+def insertUser(request):
     try:
         if request.method == 'POST':
-            received_data = json.loads(request.body)
-            selectedContent_value = received_data.get('selectedContent', '');
-            userFeedback_value = received_data.get('userFeedback', '');
-            if(selectedContent_value and userFeedback_value):
-                new_entry = sampleModel(selectedContent=selectedContent_value,
+            receivedData = json.loads(request.body)
+            selectedContent_value = receivedData.get('selectedContent', '')
+            confidence_value = receivedData.get('confidence')
+            imageURL_value = receivedData.get('imageURL')
+            sourceURL_value = receivedData.get('sourceURL')
+            proofURL = receivedData.get('proofURL')
+            userFeedback_value = receivedData.get('userFeedback', '')
+            newEntry = userSelected(selectedContent=selectedContent_value,
                     userFeedback=userFeedback_value);
-                new_entry.save()
-                return JsonResponse({'message': 'Post stored successfully!'})
-            else:
-                return JsonResponse({'error': 'Required fields are empty'}, status=400)
+            newEntry.save()
+            return JsonResponse({'message': 'Post stored successfully!'})
         else:
             return JsonResponse({'message': 'Invalid request method'}, status=400)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 
-def get_data(request):
+def insertNews(request):
+    try:
+        if request.method == 'POST':
+            receivedData = json.loads(request.body)
+            sourceURL_value = receivedData.get('sourceURL')
+            articleTitle_value = receivedData.get('articleTitle')
+            articleContent_value = receivedData.get('articleContent')
+            imageURL_value = receivedData.get('imageURL')
+            newEntry = newsArticles(sourceURL=sourceURL_value,
+                                    articleTitle=articleTitle_value,
+                                    articleContent=articleContent_value,
+                                    imageURL=imageURL_value);
+            newEntry.save()
+            return JsonResponse({'message': 'Post stored successfully!'})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+def insertSocial(request):
+    try:
+        if request.method == 'POST':
+            receivedData = json.loads(request.body)
+            sourceURL_value = receivedData.get('sourceURL')
+            articleContent_value = receivedData.get('articleContent')
+            imageURL_value = receivedData.get('imageURL')
+            platformName_value = receivedData.get('platformName')
+            newEntry = socialMediaPosts(sourceURL=sourceURL_value,
+                                    articleContent=articleContent_value,
+                                    imageURL=imageURL_value,
+                                    platformName=platformName_value);
+            newEntry.save()
+            return JsonResponse({'message': 'Post stored successfully!'})
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+def getData(request):
     try:
         if request.method == 'GET':
             selectedContent_value = request.GET.get('selectedContent', '');
-            row = sampleModel.objects.get(selectedContent=selectedContent_value)
-            return JsonResponse({'result': row.result})
+            row = userSelected.objects.get(selectedContent=selectedContent_value)
+            return JsonResponse({'result': row.status})
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
