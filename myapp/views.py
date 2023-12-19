3c6ea17d4285e0cb4af936e4b1f51cc8b503be75
dangolfecho <55153800+dangolfecho@users.noreply.github.com>
2023-12-19 10:47:10 +0530

    Update views.py - Added view to gen JWT

--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,11 @@ import json
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken , RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
 
 # Create your views here.
 @api_view(['POST'])
@@ -67,9 +72,9 @@ def insertSocial(request):
                                     imageURL=imageURL_value,
                                     platformName=platformName_value);
             newEntry.save()
-            return JsonResponse({'message': 'Post stored successfully!'})
+            return Response({'message': 'Post stored successfully!'})
     except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+        return Response({'error': str(e)}, status=500)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -81,3 +86,37 @@ def getData(request):
             return Response({'result': row.status})
     except Exception as e:
         return Response({'error': str(e)}, status=500)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def generate_jwt_token(request):
+    try:
+        if(request.method == 'POST'):
+            if(request.user):
+                token = AccessToken.for_user(request.user)
+                print("HERE")
+                jwt_token = str(token)
+                return Response({'access': jwt_token})
+            else:
+                return Response({'error': 'User auth failed'})
+    except Exception as e:
+        return Response({'error': str(e)}, status=500)
+
+class TokenObtainView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({'error': 'Please provide both username and password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'access': str(refresh.access_token),
+                'refresh_token': str(refresh)
+            })
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
